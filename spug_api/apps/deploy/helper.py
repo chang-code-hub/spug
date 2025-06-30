35e7571fa701bfc8033a6c6b4d6eefb26b9bfd52
Liu Chang <liuchang_m@163.com>
2025-07-01 01:14:57 +0800

    add pre command

--- spug_api/apps/deploy/helper.py
+++ spug_api/apps/deploy/helper.py
@@ -273,7 +273,12 @@ class Helper:
         if env:
             env = dict(env.items())
             env.update(os.environ)
-        task = subprocess.Popen(command, env=env, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, executable='/bin/bash')
+
+        exec_command = f'''
+[ -f /root/command.sh ] && . /root/command.sh
+{command}
+        '''
+        task = subprocess.Popen(exec_command, env=env, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, executable='/bin/bash')
         message = b''
         while True:
             output = task.stdout.read(1)
